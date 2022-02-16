a366ddf1210d0d6e059f53b43b1a84a4907597af
Sandeep Thukral <sandeepthukral@gmail.com>
2022-02-16 23:55:24 +0100

    added key to Words render method

--- src/components/Words.tsx
+++ src/components/Words.tsx
@@ -1,7 +1,7 @@
 type WordProps = { words: string[] }
 const Words = ({ words }: WordProps) => {
 
-    const list = words.map((word: any) => <div className="word">{word}</div>)
+    const list = words.map((word: any) => <div className="word" key={word}>{word}</div>)
     return (
         <div className="words">
             <span>Total words <span id="countOfResults">{words.length}</span></span>
